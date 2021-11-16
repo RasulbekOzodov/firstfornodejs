edae69cf6d87a9e14a85b046a0cf6f4871c5ae92
RasulbekOzodov <94472707+RasulbekOzodov@users.noreply.github.com>
2021-11-16 22:19:35 +0500

    Add files via upload

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+a = '_ _ _'
+b = '_ _ _'
+c = '_ _ _'
+
+console.log('This is new game. If you choose true number you can give your smartphone. You have only 3 attempts.');
+console.log(a);
+console.log(b);
+console.log(c);
+console.log('Choose your number from 1 untill 9');
